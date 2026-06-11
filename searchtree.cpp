b7dd8e093653c3510a284fb7af015906054c8009
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-06-11 19:36:19 +0700

    looping if parent null

--- searchtree.cpp
+++ searchtree.cpp
@@ -41,3 +41,9 @@ public:
         Node *currentNode = NULL;
 
         search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
