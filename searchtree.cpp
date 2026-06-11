f0036e35eb49b9a475af52e6ea65dd83aac92949
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-06-11 19:42:14 +0700

    void inorder

--- searchtree.cpp
+++ searchtree.cpp
@@ -74,3 +74,19 @@ public:
                 currentNode = currentNode->rightchild;
         }
     }
+
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->rightchild;
+            inorder(ptr->rightchild);
+        }
+    }
